06909ce4c69eddefb215db197e051dc71a44f2b2
Roshan jha <141396860+roshan-jha-23@users.noreply.github.com>
2023-10-03 22:47:41 +0530

    Add files via upload

---
+++ BinaryTree.java
@@ -0,0 +1,71 @@
+class BinaryTree{
+   public BinaryTree(){
+     
+   }
+  private static class Node{
+    int value;
+    Node left;
+    Node right;
+    public Node(int value){
+      this.value=value;
+    }
+  }
+  private Node root;
+  //insert elemnts
+  public void populate(Scanner scanner){
+ System.out.println("enter the root node value");
+    int value=scanner.nextInt();
+    root=new Node(value);
+    populate(scanner,root);
+  }
+  private void populate(Scanner scanner,Node node){
+    System.out.println("do you want to enter left of " + node.value);
+    boolean left=scanner.nextBoolean();
+    if(left){
+       System.out.println("enter the value to the left of this node " + node.value);
+      int value=scanner.nextInt();
+      node.left=new Node(value);
+      populate(scanner,node.left);
+    }
+    System.out.println("do you want to enter right of " + node.value);
+    boolean right=scanner.nextBoolean();
+    if(right){
+       System.out.println("enter the value to the right of this node " + node.value);
+      int value=scanner.nextInt();
+      node.right=new Node(value);
+      populate(scanner,node.right);
+    }
+    
+  }
+  //display function
+  public void display(Node root){
+    display(root,"");
+  }
+  private void display(Node node,String indent){
+    if(node==null){
+      return;
+    }
+    System.out.println(indent+node.value);
+    display(node.left+indent+'\t');
+    display(node.right +indent+'\t');
+      }
+public void prettyDisplay(){
+  prettyDisplay(root,0);
+}
+  private void prettyDisplay(Node node ,int level){
+    if(node==null){
+      return;
+    }
+    prettyDisplay(node.right,level+1);
+    if(level!=0){
+      for(int i=0;i<level-1;i++){
+              System.out.println("|\t\t");
+      }
+            System.out.println("------->"+node.value);
+    }else{
+      System.out.println(node.value);
+    }
+      prettyDisplay(node.left,level+1);
+  }
+  
+}
